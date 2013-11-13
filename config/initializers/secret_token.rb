fb2e2cbd2b90fa38b5dab6c42886673c56fb962a
Kyle Alonius <k_alonius@hotmail.com>
2013-11-13 16:13:49 -0600

    First Commit, default Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoyagerWebStore::Application.config.secret_token = '5e0948a27b42267fdcb5fb42d1b18bac8f7c3401c07105f27c46a4d4995dfb6696245527eba7ba4d9671824d4e80aec27df865c828f5cacd17f38f8e1de1c17e'
