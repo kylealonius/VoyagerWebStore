6f433be3e515ff048ecb2d0a7c21ba2a29df0776
Kyle Alonius <k_alonius@hotmail.com>
2013-11-14 13:03:12 -0600

    Created a customer model and migrated the database

---
+++ db/migrate/20131114190204_create_customers.rb
@@ -0,0 +1,15 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :postal_code
+      t.string :email
+      t.integer :province_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114185219) do
+ActiveRecord::Schema.define(:version => 20131114190204) do
+
+  create_table "customers", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "postal_code"
+    t.string   "email"
+    t.integer  "province_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "provinces", :force => true do |t|
     t.string   "name"
