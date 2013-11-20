eeb28b88a58c2264d09fc53d76757756afc5e38a
Kyle Alonius <k_alonius@hotmail.com>
2013-11-20 10:26:39 -0600

    Dropped the products table in the db. Added the Product model with the category column and migrated the db.

---
+++ db/migrate/20131120161939_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.decimal :price
+      t.integer :stock_quantity
+      t.boolean :sale
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114191619) do
+ActiveRecord::Schema.define(:version => 20131120161939) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20131114191619) do
     t.decimal  "price"
     t.integer  "stock_quantity"
     t.boolean  "sale"
+    t.string   "category"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
